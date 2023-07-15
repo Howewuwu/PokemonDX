f28dc45eb1436b26e005282bc28bd6430ae2a910
Howe <howewu1991@gmail.com>
2023-07-15 15:00:29 +0800

    Initial Commit

---
+++ PokemonDX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonDX
+//
+//  Created by Howe on 2023/7/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
